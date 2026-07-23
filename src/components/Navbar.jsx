3ea2aac4615d7cc214855c29a52296e8132b0d25
GulnazRind <hyderabadwristtitans@gmail.com>
2026-07-23 03:58:48 -0700

    new some

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { RiHomeLine, RiFileListLine, RiUserAddLine, RiAdminLine } from '@remixicon/react';
 
 const Navbar = () => {
   const location = useLocation();
@@ -8,26 +9,31 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
-          HYDERABADI <span>BOYZ</span>
+          <span className="brand-icon">💪</span>
+          Hyderabadi <span>Boyz</span>
         </Link>
         <ul className="nav-links">
           <li>
             <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+              <RiHomeLine size={18} />
               Home
             </Link>
           </li>
           <li>
             <Link to="/register" className={location.pathname === '/register' ? 'active' : ''}>
+              <RiUserAddLine size={18} />
               Register
             </Link>
           </li>
           <li>
             <Link to="/matches" className={location.pathname === '/matches' ? 'active' : ''}>
+              <RiFileListLine size={18} />
               Matches
             </Link>
           </li>
           <li>
             <Link to="/admin" className={location.pathname === '/admin' ? 'active' : ''}>
+              <RiAdminLine size={18} />
               Admin
             </Link>
           </li>
